641d83c44704a929aadb5dac4d927b2191135fde
Ashley <ashleynavo@googlemail.com>
2022-04-15 22:43:45 +0100

    added .env and .env.test

--- src/app.js
+++ src/app.js
@@ -2,7 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
+
+app.get('/', (req,res) => {
+    res.send('Hello World!')
+    res.status(200)
+
+})
 
 
 module.exports = app;
