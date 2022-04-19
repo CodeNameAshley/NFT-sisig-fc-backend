94d8290b1f931855f3461d9854693c3077cf6e58
Ashley <ashleynavo@googlemail.com>
2022-04-19 21:57:05 +0100

    updated blog post test added

---
+++ tests/update-post.test.js
@@ -0,0 +1,58 @@
+const {expect} = require('chai');
+const request = require('supertest');
+const getDb = require('../src/services/db');
+const app = require('../src/app');
+
+describe('update post', () => {
+    let db
+    let blogs
+
+    beforeEach(async () => {
+        db = await getDb();
+        await Promise.all([
+            db.query('INSERT INTO Blogs (title, blog) VALUES (?, ?)', [
+                'Second Post',
+                'This is my second post',
+            ]),
+
+            db.query('INSERT INTO Blogs (title, blog) VALUES (?, ?)', [
+                'Third Post',
+                'This is my third post',
+            ]),
+
+            db.query('INSERT INTO Blogs (title, blog) VALUES (?, ?)', [
+                'Fourth Post',
+                'This is my fourth post',
+            ]),
+        ]);
+
+        [blogs] = await db.query('SELECT * FROM Blogs')
+    })
+
+    afterEach(async () => {
+        await db.query('DELETE FROM Blogs');
+        await db.close();
+    })
+
+    describe('/blogs/:blogID', () => {
+        describe('PATCH', () => {
+            it('updates a single blog post with the correct ID', async () => {
+
+                const blog = blogs[0];
+                const res = await request(app).patch(`/blogs/${blog.id}`).send({title: 'updated title', blog: 'updated blog body'})
+    
+                expect(res.status).to.equal(200);
+    
+                const [[newBlogPost]] = await db.query('SELECT * FROM Blogs WHERE id = ?', [blog.id])
+    
+                expect(newBlogPost).to.equal.apply('updated title')
+            })
+        })
+
+        it('returns a 404 if the blog post is not in the database', async () => {
+            const res = await request(app).patch('/blogs/12345').send({name: 'updated title'});
+
+            expect(res.status).to.equal(404);
+        })
+    })
+})
