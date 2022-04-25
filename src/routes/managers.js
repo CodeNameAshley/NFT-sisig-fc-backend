42c5007159db0859454aa55d5794496e97f89592
Ashley <ashleynavo@googlemail.com>
2022-04-25 22:22:22 +0100

    Read Managers by ID test passed, router and controller created

--- src/routes/managers.js
+++ src/routes/managers.js
@@ -5,6 +5,7 @@ const managers = require('../controllers/managers');
 
 router.post('/', managers.createManager)
 router.get('/', managers.readAllManagers)
+router.get('/:managerId', managers.readByManagerId);
 router.patch('/:managerId', managers.updateByManagerId);
 
 module.exports = router;
